b2926e2af56ab48ae126f56643daef5bcb726b1e
pknull <louis.grenzebach@gmail.com>
2018-08-21 20:40:30 -0700

    Removed needles examples, added the ability for unique toss results

--- flipper/Casts.py
+++ flipper/Casts.py
@@ -1,3 +1,5 @@
+# sample sides
+
 class Coin:
     SIDES = ['Heads', 'Tails']
 
@@ -8,15 +10,3 @@ class EightBall:
              'It is decidedly so', 'Better not tell you now', 'Very doubtful', 'Yes - definitely',
              'It is certain', 'Cannot predict now', 'Most likely.', 'Ask again later', 'My reply is no',
              'Outlook good', 'Don\'t count on it', 'Yes, in due time', 'My sources say no']
-
-class Killer:
-    SIDES = ['Trapper', 'Wraith', 'Hillbilly', 'Nurse', 'Huntress', 'Shape', 'Hag', 'Doctor', 'Pig', 'Clown',
-             'Freddy', 'Leatherface']
-
-class Defender:
-    SIDES = ["Caviera", "Ela", "Vigil", "Echo", "Valkyrie", "Maestro", "Tachanka", "Alibi", "Lesion", "Jäger",
-             "Frost", "Mira", "Smoke", "Bandit", "Mute", "Rook", "Kapkan", "Recruit", "Doc", "Pulse", "Castle"]
-
-class Attacker:
-    SIDES = ["Sledge", "Thatcher", "Ash", "Thermite", "Twitch", "Montagne", "Glaz", "Fuze", "Blitz", "IQ", "Buck",
-             "Blackbeard", "Capitão", "Hibana", "Jackal", "Ying", "Zofia", "Dokkaebi", "Lion", "Finka", "Recruit"]
--- flipper/Tosser.py
+++ flipper/Tosser.py
@@ -4,10 +4,16 @@ class Tosser:
     def __init__(self, castee):
         self.castee = castee
 
-    def toss(self, ntoss=1):
-        result = []
-        for x in range(0, ntoss):
-            result.append(random.choice(self.castee.SIDES))
+    def toss(self, ntoss=1, unique=False):
+        if unique:
+            if ntoss > len(self.castee.SIDES):
+                result = self.castee.SIDES
+            else:
+                result = random.sample(self.castee.SIDES, ntoss)
+        else:
+            result = []
+            for x in range(0, ntoss):
+                result.append(random.choice(self.castee.SIDES))
         return result
 
 
